64664c897c7d8a4e1271ba05e7f358c9148b8667
brian <brianbb174@protonmail.com>
2020-05-27 18:20:08 -0700

    added more validation

--- phone.py
+++ phone.py
@@ -7,8 +7,22 @@ def extract_phone(input):
         return match.group()
     raise ValueError("must be formatted")
 
+def extract_all_phone(input):
+    phone_regex = re.compile(r'\b\d{3} \d{3} \d{4}\b')
+    return phone_regex.findall(input)
     
+    
+def is_valid_phone(input):
+    phone_regex = re.compile(r'^\b\d{3} \d{3} \d{4}$')
+    match = phone_regex.search(input)
+    if match:
+        return True
+    return False
 
-# def is_valid_phone():
-print(extract_phone("this is a new 623 623 6235"))
-print(extract_phone("this is a new 623 623 62355"))
+
+print(is_valid_phone("423 623 6235"))
+
+
+
+# print(extract_phone("this is a new 623 623 6235"))
+print(extract_all_phone("this is a new 623 623 6235 or 815 234 2345"))
